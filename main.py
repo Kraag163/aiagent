f6ee0834b82db17ecefd10e28d3dc07d2a52e22c
Kraag163 <mulder163@yahoo.fr>
2025-10-30 18:32:18 +0100

    CH2 L1 done

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+
 import sys
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+
+
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,20 +15,27 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 
-
 def main():
-    print("Hello from aiagent!")
 
-if len(sys.argv) < 2: 
-  print("Error") 
-  sys.exit(1) 
+    #print("Hello from aiagent!")
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=sys.argv[1]
-)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+]
+
+    if len(sys.argv) < 2: 
+        print("Error") 
+        sys.exit(1) 
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=messages
+    )
+    print(response.text)
+    
+    if "--verbose" in sys.argv[1:]:
+        print(f"User prompt: {sys.argv[1]}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":

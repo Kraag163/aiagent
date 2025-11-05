f6f3863e3645b168b6302e648d246507451cea1e
Kraag163 <mulder163@yahoo.fr>
2025-11-05 22:16:43 +0100

    CH4 L3 done

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from functions.get_files_info import *
 from functions.get_file_content import *
 from functions.run_python_file import *
 from functions.write_file import *
+from functions.call_function import call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -49,36 +50,67 @@ All paths you provide should be relative to the working directory. You do not ne
         ]
     )
 
-    try:
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages,
-            config=types.GenerateContentConfig(
-                tools=[available_functions],
-                system_instruction=system_prompt,
-            ),
-        )
-    except Exception as e:
-        print(f"API error: {e}")
-        sys.exit(1)
+    for iteration in range(0, 20):
 
-    calls = response.function_calls or []
-    if calls:
-        for f in calls:
-            print(f"Calling function: {f.name}({f.args})")
+        try:
+            response = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions],
+                    system_instruction=system_prompt,
+                ),
+            )
+        except Exception as e:
+            print(f"API error: {e}")
+            sys.exit(1)
+
+        
+        for cand in response.candidates:
+            messages.append(cand.content)
+
+        verbose = "--verbose" in sys.argv[1:]
+
+        calls = response.function_calls or []
+        if calls:
+            for f in calls:
+                print(f" - Calling function: {f.name}")
+                tool_content = call_function(f, verbose=verbose)
+
+                if not tool_content.parts or not getattr(tool_content.parts[0], "function_response", None):
+                    raise RuntimeError("Tool response missing function_response")
+
+                result_obj = tool_content.parts[0].function_response.response
+
+                message_to_append = types.Content(role="user",
+                    parts=[
+                        types.Part(
+                            function_response=types.FunctionResponse(
+                                name=f.name,
+                                response=result_obj
+                ))])
+                messages.append(message_to_append)
+
+                if verbose:
+                    print(f"-> {result_obj}")
+                
+            continue
+
+        if response.text:
+            print("Final response:")
+            print(response.text)
+            break
+        
+            
+        if "--verbose" in sys.argv[1:]:
+            print(f"User prompt: {sys.argv[1]}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     else:
-        print(response.text)
+        # loop exhausted
+        print("No final response after max iterations.")   
 
-    # if len(response.function_calls) !=0:
-    #     for f in response.function_calls:
-    #         print(f"Calling function: {f.name}({f.args})")
-    # else:
-    #     print(response.text)
-
-    if "--verbose" in sys.argv[1:]:
-        print(f"User prompt: {sys.argv[1]}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
